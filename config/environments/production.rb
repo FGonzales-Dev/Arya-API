c0aa00c410a6f111e038596bc696c33a8171a880
Francis Gonzales <francisgonzales.private@gmail.com>
2020-06-01 13:43:28 +0800

    added config to preoduction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.assets.initialize_on_precompile = false
   config.assets.compile = true
 
   # Eager load code on boot. This eager loads most of Rails and
